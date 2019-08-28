ae4c5d9c34f206a97ec85e7b0aa14ab77db62cac
蔡承宇 <chengyucai@liontravel.com>
2019-08-28 14:00:03 +0800

    -----

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 // next.config.js
-const path = require("path");
-const merge = require("webpack-merge");
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
+const path = require('path');
+const merge = require('webpack-merge');
+const withSass = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
 
-const root = path.resolve("./");
-const common = require(path.resolve(root, "config/webpack.commons.js"));
+const root = path.resolve('./');
+const common = require(path.resolve(root, 'config/webpack.commons.js'));
 
 module.exports = withCSS(
-  withSass({
-    distDir: process.env.NODE_ENV === "production" ? "proBuild" : ".next",
-    generateInDevMode: false,
-    webpack(config) {
-      const mergeConfig = merge(common, config);
-      return mergeConfig;
-    }
-  })
+    withSass({
+        distDir: process.env.NODE_ENV === 'production' ? 'proBuild' : '.next',
+        generateInDevMode: false,
+        webpack(config) {
+            const mergeConfig = merge(common, config);
+            return mergeConfig;
+        },
+    }),
 );
--- server.js
+++ server.js
@@ -1,64 +1,60 @@
-require("dotenv").config();
-const next = require("next");
-const targetFolder =
-  process.env.NODE_ENV === "production" ? "proBuild" : ".next";
+require('dotenv').config();
+const next = require('next');
+const targetFolder = process.env.NODE_ENV === 'production' ? 'proBuild' : '.next';
 
 const app = next({
-  dev: process.env.NODE_ENV !== "production"
+    dev: process.env.NODE_ENV !== 'production',
 });
 
 const handler = app.getRequestHandler();
-const express = require("express");
+const express = require('express');
 
 const server = express();
 
 const port =
-  process.env.NODE_ENV !== "production"
-    ? process.env.DEVELOPMENT_PORT || 3000
-    : process.env.PRODUCTION_PORT || 80;
+    process.env.NODE_ENV !== 'production' ? process.env.DEVELOPMENT_PORT || 3000 : process.env.PRODUCTION_PORT || 80;
 
-const detect = require("detect-port");
+const detect = require('detect-port');
 
 const detect_port = function(port) {
-  detect(port, (err, _port) => {
-    if (err) {
-      console.log(err);
-    }
-    if (port == _port) {
-      console.log(`port: ${port} was not occupied`);
-      this.listen(port, err => {
-        if (err) throw err;
-        console.log(`> Ready on http://localhost:${port}`);
-      });
-    } else {
-      console.log(`port: ${port} was occupied, try port: ${_port}`);
-      detect_port.call(this, _port);
-    }
-  });
+    detect(port, (err, _port) => {
+        if (err) {
+            console.log(err);
+        }
+        if (port == _port) {
+            console.log(`port: ${port} was not occupied`);
+            this.listen(port, err => {
+                if (err) throw err;
+                console.log(`> Ready on http://localhost:${port}`);
+            });
+        } else {
+            console.log(`port: ${port} was occupied, try port: ${_port}`);
+            detect_port.call(this, _port);
+        }
+    });
 };
 
-app
-  .prepare()
-  .then(() => {
-    // 測試環境開啟 api SERVER
-    // if (process.env.NODE_ENV === 'development') {
+app.prepare()
+    .then(() => {
+        // 測試環境開啟 api SERVER
+        // if (process.env.NODE_ENV === 'development') {
 
-    const cors = require("cors");
-    const apiRouter = require("./test/data");
+        const cors = require('cors');
+        const apiRouter = require('./test/data');
 
-    server.use("/api", apiRouter(express.Router()));
+        server.use('/api', apiRouter(express.Router()));
 
-    server.use(cors());
+        server.use(cors());
 
-    server.use(express.static(targetFolder));
+        server.use(express.static(targetFolder));
 
-    detect_port.call(server, port);
+        detect_port.call(server, port);
 
-    server.get("*", (req, res) => {
-      return handler(req, res);
+        server.get('*', (req, res) => {
+            return handler(req, res);
+        });
+    })
+    .catch(ex => {
+        console.error(ex.stack);
+        process.exit(1);
     });
-  })
-  .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
