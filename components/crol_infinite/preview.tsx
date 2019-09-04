09577be1f43b4ee4f2e9d97c5e4bdf8fcd97db16
蔡承宇 <chengyucai@liontravel.com>
2019-09-04 08:34:50 +0800

    add crol_infinite

---
+++ components/crol_infinite/preview.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Module from './index';
+
+import { withKnobs, text, boolean, object, number, select } from '@storybook/addon-knobs';
+storiesOf('carousel(輪播)', module)
+    .addDecorator(withKnobs)
+    .add('crol_infinite', () => {
+        const props = {
+            img_list: object('img_list', [
+                'https://picsum.photos/500/300',
+                'https://picsum.photos/600/500',
+                'https://picsum.photos/700/500',
+                'https://picsum.photos/500/500',
+                'https://picsum.photos/600/300',
+                'https://picsum.photos/600/400',
+                'https://picsum.photos/500/700',
+                'https://picsum.photos/700/700',
+            ]),
+        };
+        return (
+            <>
+                <Module {...props}></Module>
+            </>
+        );
+    });
--- config/funtion.js
+++ config/funtion.js
@@ -1,3 +1,17 @@
 module.exports.Random = function(max, min = 1000) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
+
+module.exports.windmill = function(
+    value,
+    length,
+    { increments = 1, turn = true, min = 0, loop = false, bcpl = true } = {},
+) {
+    length = typeof length == 'number' ? length : length.length;
+    turn ? (value += increments) : (value -= increments);
+
+    if (value > (bcpl ? length - 1 : length)) value = loop ? min : bcpl ? length - 1 : length;
+    if (value < min) value = loop ? (bcpl ? length - 1 : length) : min;
+
+    return value;
+};
